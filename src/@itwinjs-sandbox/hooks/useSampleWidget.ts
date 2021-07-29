cf886911014bfee5d0bfcad99b9a6ee386d71e90
Stefan Retief <stefan.retief@me.com>
2021-07-29 09:15:30 -0700

    fixed issue with hooks updating twice.

--- src/@itwinjs-sandbox/hooks/useSampleWidget.ts
+++ src/@itwinjs-sandbox/hooks/useSampleWidget.ts
@@ -2,10 +2,6 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-
-import { RegisteredRuleset, Ruleset } from "@bentley/presentation-common";
-import { Presentation } from "@bentley/presentation-frontend";
-import * as HILITE_RULESET from "@bentley/presentation-frontend/lib/presentation-frontend/selection/HiliteRules.json";
 import { UiItemsManager } from "@bentley/ui-abstract";
 import { SampleWidgetProvider } from "@itwinjs-sandbox/components/imodel-selector/SampleWidgetProvider";
 import { defaultIModelList } from "@itwinjs-sandbox/constants";
@@ -16,29 +12,6 @@ import { SampleIModelInfo, useSampleIModelConnection } from "./useSampleIModelCo
 
 export const useSampleWidget = (instructions: string, iModelList: (SampleIModels | SampleIModelWithAlternativeName)[] = defaultIModelList): SampleIModelInfo | undefined => {
   const [sampleIModelInfo, setIModelName] = useSampleIModelConnection(iModelList);
-
-  /**
-   * Fix to add Hilite ruleset to presentation until Bug #599922 is addressed
-   */
-  useEffect(() => {
-    const addHiliteRuleset = async () => {
-      try {
-        if (Presentation.presentation) {
-          const ruleset: RegisteredRuleset | undefined = await Presentation.presentation.rulesets().get("presentation-frontend/HiliteRules");
-          if (!ruleset) {
-            await Presentation.presentation.rulesets().add((HILITE_RULESET as any).default as Ruleset);
-          }
-        }
-      } catch {
-        // Presentation not initialized
-      }
-    };
-    addHiliteRuleset()
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error(error);
-      });
-  });
 
   useEffect(() => {
     const floatingWidgets = new FloatingWidgets("sample-container");
